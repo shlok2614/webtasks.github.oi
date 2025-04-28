c536ce09d98195123bf793ccc176554e3c6f0eb4
shlok2614 <shloksg2614@gmail.com>
2025-04-28 23:10:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
 st.title('Fake News Detector')
-input_text = st.text_input('Enter news Article')  
+input_text = st.text_input('Enter news Article')
+
+def prediction(input_text):
+    input_text = vector.transform([input_text])
+    prediction = model.predict(input_text)
+    return prediction[0]
+
+if input_text:
+    pred = prediction(input_text)
+    if pred == 1:
+        st.write('The News is Fake')
+    else :
+        st.write('The News is Real')
