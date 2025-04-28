f419e54a54499eac3d0e1937d9863263bba49453
shlok2614 <shloksg2614@gmail.com>
2025-04-28 23:30:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import streamlit as st
-import pickle
 
-vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
-
-st.title('Fake News Detector')
-input_text = st.text_input('Enter news Article')
-
-def prediction(input_text):
-    input_text = vector.transform([input_text])
-    prediction = model.predict(input_text)
-    return prediction[0]
 
 if input_text:
     pred = prediction(input_text)
